b239cd17c49b6227561084ea5c96a98dd1ba4785
kishoreyanabathina <kishoreyanabathina@gmail.com>
2024-01-25 00:08:59 -0500

    Kishore_completed llmragdemo project

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 import requests
 
-API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
-API_URL = "" #Add a URL for a model of your choosing
+#API_TOKEN = os.environ["API_TOKEN"] #Set a API_TOKEN environment variable before running
+#API_URL = "" #Add a URL for a model of your choosing
+API_TOKEN = "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"
+API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 def query(prompt):
     payload = {
-        "inputs": prompt,
+        #"inputs": prompt,
+        #"inputs": "What is 2+2? Give me a concise answer.",
+        "inputs": "What is the population of Jacksonville, FL?",
         "parameters": { #Try and experiment with the parameters
             "max_new_tokens": 1024,
             "temperature": 0.6,
